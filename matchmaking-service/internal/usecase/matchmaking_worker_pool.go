975a453219051089eb333dd5f4f5fd28549998f1
locne <nakrothnguyen127@gmail.com>
2025-08-11 16:13:54 +0700

    feat(matchmaking-service): add getPlayerElo HTTP call to player-service and integrate elo fetching in worker pool

--- matchmaking-service/internal/usecase/matchmaking_worker_pool.go
+++ matchmaking-service/internal/usecase/matchmaking_worker_pool.go
@@ -2,6 +2,10 @@ package usecase
 
 import (
     "fmt"
+    "net/http"
+    "encoding/json"
+    "bytes"
+    "io/ioutil"
 )
 
 type MatchmakingJob struct {
@@ -25,8 +29,39 @@ func NewWorkerPool(poolManager *PoolManager, workerCount int) *WorkerPool {
     return wp
 }
 
+func getPlayerElo(playerServiceURL string, userID int, gameType string) (int, error) {
+    reqBody := map[string]interface{}{
+        "user_id":  userID,
+        "game_type": gameType,
+    }
+    bodyBytes, _ := json.Marshal(reqBody)
+    resp, err := http.Post(playerServiceURL+"/api/v1/player/elo", "application/json", bytes.NewBuffer(bodyBytes))
+    if err != nil {
+        return 0, err
+    }
+    defer resp.Body.Close()
+    if resp.StatusCode != http.StatusOK {
+        return 0, fmt.Errorf("player-service returned status %d", resp.StatusCode)
+    }
+    respBody, _ := ioutil.ReadAll(resp.Body)
+    var result map[string]interface{}
+    if err := json.Unmarshal(respBody, &result); err != nil {
+        return 0, err
+    }
+    elo, ok := result["elo"].(float64)
+    if !ok {
+        return 0, fmt.Errorf("elo not found in response")
+    }
+    return int(elo), nil
+}
+
 func (wp *WorkerPool) worker() {
     for job := range wp.Jobs {
+        playerServiceURL := "http://localhost:3002"
+        elo, err := getPlayerElo(playerServiceURL, job.Player.UserId, job.Player.GameType)
+        if err == nil {
+            job.Player.Elo = elo
+        }
         opponent := wp.PoolManager.FindNearestElo(job.PoolKey, job.Player.Elo)
         if opponent != nil {
             // to do MQ
